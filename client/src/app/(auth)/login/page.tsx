ee89bb54c80b127cd1d0f1bdfb8501c2d9404f53
swadesh-231 <swadeshchatterjee512@gmail.com>
2026-07-14 12:08:55 +0530

    create login and signup page and hero section

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -1,10 +1,33 @@
-
-
+import { SubmitButton } from "@/components/common/SubmitButton";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <form>
+      <div className="mt-4">
+        <Label htmlFor="email">Email</Label>
+        <Input placeholder="Type your email" name="email" />
+        <span className="text-red-400">{}</span>
+      </div>
+      <div className="mt-4">
+        <Label htmlFor="password">Password</Label>
+        <Input
+          type="password"
+          placeholder="Type your password"
+          name="password"
+        />
+        <div className="text-right font-bold">
+          <Link href="/forgot-password">Forgot Password?</Link>
+        </div>
+        <span className="text-red-400">{}</span>
+      </div>
+      <div className="mt-4">
+        <SubmitButton />
+      </div>
+    </form>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
--- client/src/app/(auth)/signup/page.tsx
+++ client/src/app/(auth)/signup/page.tsx
@@ -1,8 +1,43 @@
+import { SubmitButton } from "@/components/common/SubmitButton"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+   <form >
+      <div className="mt-4">
+        <Label htmlFor="name">Name</Label>
+        <Input placeholder="Type your name" name="name" />
+        <span className="text-red-400">{}</span>
+      </div>
+      <div className="mt-4">
+        <Label htmlFor="email">Email</Label>
+        <Input placeholder="Type your email" name="email" />
+        <span className="text-red-400">{}</span>
+      </div>
+      <div className="mt-4">
+        <Label htmlFor="password">Password</Label>
+        <Input
+          type="password"
+          placeholder="Type your password"
+          name="password"
+        />
+        <span className="text-red-400">{}</span>
+      </div>
+      <div className="mt-4">
+        <Label htmlFor="cpassword">Confirm Password</Label>
+        <Input
+          type="password"
+          placeholder="Type your password"
+          name="confirm_password"
+        />
+        <span className="text-red-400">{}</span>
+      </div>
+      <div className="mt-4">
+        <SubmitButton />
+      </div>
+    </form>
   )
 }
 
