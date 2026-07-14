a99562bb65d3f221ec9273820432282b60da90e7
swadesh-231 <swadeshchatterjee512@gmail.com>
2026-07-14 12:16:49 +0530

    set up ejs

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,17 @@
 import express from "express";
 import "dotenv/config";
 import type { Application, Request, Response } from "express";
+import * as path from "path";
+import { fileURLToPath } from "url";
 
 const app: Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+// * Set View engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
